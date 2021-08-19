2c23b5159a65d3377bf2a0953b670c9042c2a5ee
Gabriel Fontes <eu@misterio.me>
2021-08-19 02:14:40 -0300

    add nix support and gitignore

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Joguinho de forca";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      name = "forca";
+    in rec {
+
+      packages.${name} = pkgs.stdenv.mkDerivation {
+        pname = name;
+        version = "1.0";
+        src = ./.;
+        installPhase = ''
+          mkdir -p $out/bin
+          mv build/${name} $out/bin
+        '';
+      };
+      defaultPackage = packages.${name};
+
+      apps.${name} = {
+        type = "app";
+        program = "${packages.${name}}/bin/${name}";
+      };
+      defaultApp = apps.${name};
+
+      devShell =
+        pkgs.mkShell { buildInputs = with pkgs; [ make gcc valgrind gdb ]; };
+    });
+}
